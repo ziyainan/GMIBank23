621693a33b8a5f908eaf726eaec5b061ab41da94
ozgur cil <ozgurcil90@gmail.com>
2020-12-10 23:31:50 -0500

    Elci abis cod pushing

---
+++ src/test/java/gmibank/runners/ElciRunner.java
@@ -0,0 +1,24 @@
+package gmibank.runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(strict = true,
+
+        plugin = {"html:target/default-cucumber-reports",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml"},
+        features = "src/test/resources/features",
+        glue = "gmibank/step_defs",
+        tags = "@LoginValid",
+        dryRun = false
+)
+
+public class ElciRunner {
+
+}
