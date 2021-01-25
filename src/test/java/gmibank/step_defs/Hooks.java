0b193187ca93be557356217fec087b8f79d803f9
Abdul Isik <sampiy10um@gmail.com>
2021-01-24 22:42:06 -0600

    Hooks + US015

--- src/test/java/gmibank/step_defs/Hooks.java
+++ src/test/java/gmibank/step_defs/Hooks.java
@@ -1,6 +1,5 @@
 package gmibank.step_defs;
 
-
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
@@ -13,36 +12,31 @@ import org.openqa.selenium.TakesScreenshot;
 import java.util.concurrent.TimeUnit;
 
 public class Hooks {
-
-    private final static Logger logger = Logger.getLogger(Hooks.class);
-
-
-    @Before
-    public void setUp() {
-        logger.info("::: Starting automation :::");
-        logger.info("Browser type: " + ConfigurationReader.getProperty("browser"));
-        logger.info("URL: " + ConfigurationReader.getProperty("login_url"));
+	
+	private final static Logger logger = Logger.getLogger(Hooks.class);
+	
+	@Before
+	public void setUp() {
+		logger.info("::: Starting automation :::");
+		logger.info("Browser type: " + ConfigurationReader.getProperty("browser"));
+		logger.info("URL: " + ConfigurationReader.getProperty("login_url"));
 
 ////    public void setUp(){
-        Driver.getDriver().manage().window().maximize();
-        Driver.getDriver().manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-        Driver.getDriver().get(ConfigurationReader.getProperty("login_url"));
-    }
-
-    @After
-    public void tearDown(Scenario scenario) {
-
-         logger.info("Taking a screenshot");
-
-        //if (scenario.isFailed()){
-
-
-        final byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
-        scenario.embed(screenshot, "image/png");
-
-        // Driver.getDriver().close();
-    }
-
-    
-
+		Driver.getDriver().manage().window().maximize();
+		Driver.getDriver().manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		Driver.getDriver().get(ConfigurationReader.getProperty("login_url"));
+	}
+	
+	@After
+	public void tearDown(Scenario scenario) {
+		
+		//if (scenario.isFailed()){
+		logger.info("Taking a screenshot");
+		final byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+		scenario.embed(screenshot, "image/png");
+		//}
+		Driver.closeDriver();
+		
+	}
+	
 }
